a536072e9702be3f488aec233abb7bfd1b436b1d
Lucas Garron <code@garron.net>
2022-07-25 14:30:51 -0700

    Add a declaration to fix a type error.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { randomScrambleForEvent } from "cubing/scramble";
 
 class App {
   // Example of getting an element from the page.
-  twistyPlayer: TwistyPlayer = document.querySelector("#main-player");
+  twistyPlayer: TwistyPlayer = document.querySelector("#main-player")!;
   // Example of creating a new element and adding it to the page.
   twistyAlgViewer = document.body.appendChild(
     new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer })
