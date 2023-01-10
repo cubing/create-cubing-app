3fb4d34e61c2f4b7aa49d84fabe5e045628f43d2
Lucas Garron <code@garron.net>
2023-01-09 18:00:50 -0800

    Fix up script.

--- bin/create-cubing-app.js
+++ bin/create-cubing-app.js
@@ -1,8 +1,7 @@
 import { exec } from "child_process";
-import { writeFile } from "fs";
-import { mkdir, readFile } from "fs/promises";
+import { mkdir, readFile, writeFile } from "fs/promises";
 
-export function execPromise(cmd, options) {
+function execPromise(cmd, options) {
 	return new Promise((resolve, reject) => {
 		const childProcess = exec(cmd, options, (error, stdout, stderr) => {
 			if (error) {
@@ -11,21 +10,33 @@ export function execPromise(cmd, options) {
 			// console.log(stdout);
 			resolve(stdout ? stdout : stderr);
 		});
-		childProcesses.push(childProcess);
 	});
 }
+
+const initialPackageJSON = {
+	scripts: {
+		build:
+			'node -e \'import("barely-a-dev-server").then(s => s.barelyServe({entryRoot: "src", dev: false, outDir: "dist/web"}))\'',
+		dev: 'node -e \'import("barely-a-dev-server").then(s => s.barelyServe({entryRoot: "src"}))\'',
+		clean: "rm -rf ./dist",
+	},
+};
+await writeFile(
+	"./package.json",
+	JSON.stringify(initialPackageJSON, null, "  "),
+);
 
 await execPromise("npm install --save cubing");
 await execPromise("npm install --save-dev barely-a-dev-server");
 
 await mkdir("./src", { recursive: true });
-async function transferFile(basename) {
+async function transferFile(rootedPath) {
 	const indexHTML = await readFile(
-		new URL(`../src/${basename}`),
-		import.meta.url,
+		new URL(`../${rootedPath}`, import.meta.url),
 	);
-	writeFile(`./src/${basename}`, indexHTML);
+	await writeFile(`./${rootedPath}`, indexHTML);
 }
-await transferFile("index.html");
-await transferFile("index.ts");
-await transferFile("index.css");
+await transferFile("src/index.html");
+await transferFile("src/main.ts");
+await transferFile("src/index.css");
+await transferFile(".gitignore");
