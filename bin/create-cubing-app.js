d344f8a821be7ba2baf662a0a830ba1ea9a6ec54
Lucas Garron <code@garron.net>
2023-01-09 19:02:44 -0800

    Move src files back so we can actuall develop in the source repo.

--- bin/create-cubing-app.js
+++ bin/create-cubing-app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { exec } from "child_process";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, readFile, writeFile } from "fs/promises";
 import { join } from "path";
 import { exit, stderr } from "process";
 
@@ -63,90 +63,26 @@ const execOptions = {
 	cwd: packageRoot,
 };
 await mkdir(packageRooted("src"), { recursive: true });
-async function transferFile(rootedPath, contents) {
+async function transferFile(rootedPath) {
+	const filePath = new URL(join("..", rootedPath), import.meta.url);
+	const contents = await readFile(filePath, "utf-8");
 	await writeFile(packageRooted(rootedPath), contents);
 }
-await transferFile(
-	"src/index.html",
-	`<!DOCTYPE html>
-<html>
-
-<head>
-  <meta charset="utf8">
-  <title>My Cubing App</title>
-  <link rel="stylesheet" href="./index.css">
-  <!-- Note: the source file is \`main.ts\`, but here we use the transpiled file name it will have on the web server. -->
-  <script src="./main.js" type="module"></script>
-</head>
-
-<body>
-  <h1>My Cubing App</h1>
-  <twisty-player id="main-player"></twisty-player>
-</body>
-
-</html>
-`,
-);
-await transferFile(
-	"src/main.ts",
-	`// Always keep the following line if you are using any twisty players on your page.
-import "cubing/twisty";
-// Use the following line for specific imports from \`cubing/twisty\`.
-import { TwistyAlgViewer, type TwistyPlayer } from "cubing/twisty";
-
-// Import from other modules as usual.
-import { randomScrambleForEvent } from "cubing/scramble";
-
-class App {
-  // Example of getting an element from the page.
-  twistyPlayer: TwistyPlayer = document.querySelector("#main-player")!;
-  // Example of creating a new element and adding it to the page.
-  twistyAlgViewer = document.body.appendChild(
-    new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer })
-  );
-  constructor() {
-    this.updateScramble();
-  }
-
-  async updateScramble() {
-    this.twistyPlayer.alg = await randomScrambleForEvent("333");
-  }
-}
-
-// Make the app object available in the console for debugging.
-// Try running: app.updateScramble()
-globalThis.app = new App();
-`,
-);
-await transferFile(
-	"src/index.css",
-	`/* Center everything on the page. */
-html, body {
-  height: 100%;
-  margin: 0;
-  display: grid;
-  place-content: center;
-  gap: 1em;
-  font-family: sans-serif;
-}
-`,
-);
-await transferFile(
-	".gitignore",
-	`/dist
-/node_modules
-`,
-);
+await transferFile("src/index.html");
+await transferFile("src/main.ts");
+await transferFile("src/index.css");
+await transferFile(".gitignore");
 
 await execPromise("npm install --save cubing", execOptions);
 await execPromise("npm install --save-dev barely-a-dev-server", execOptions);
 
-console.log(`Created a cubing project. To develop your app, run:
+console.log(`Your cubing app has been created.
+To work on it, run:
 
     cd ${packageRoot}
     npm run dev
 
-To create a build of your app that can be uploaded to a file server, run:
+To create an optimized build of your app that can be uploaded to a file server, run:
 
     npm run build
 
