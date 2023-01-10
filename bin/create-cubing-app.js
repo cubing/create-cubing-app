9a0fc8494c386d6945821cf82611971d81a9a7a9
Lucas Garron <code@garron.net>
2023-01-09 18:10:44 -0800

    🥳 INLINING PARTY 🥳

--- bin/create-cubing-app.js
+++ bin/create-cubing-app.js
@@ -31,11 +31,78 @@ await execPromise("npm install --save cubing");
 await execPromise("npm install --save-dev barely-a-dev-server");
 
 await mkdir("./src", { recursive: true });
-async function transferFile(rootedPath) {
+async function transferFile(rootedPath, contents) {
 	const indexHTML = await readFile(new URL(`./${rootedPath}`, import.meta.url));
 	await writeFile(`./${rootedPath}`, indexHTML);
 }
-await transferFile("src/index.html");
-await transferFile("src/main.ts");
-await transferFile("src/index.css");
-await transferFile(".gitignore");
+await transferFile(
+	"src/index.html",
+	`<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf8">
+  <title>My Cubing App</title>
+  <link rel="stylesheet" href="./index.css">
+  <!-- Note: the source file is \`main.ts\`, but here we use the transpiled file name it will have on the web server. -->
+  <script src="./main.js" type="module"></script>
+</head>
+
+<body>
+  <h1>My Cubing App</h1>
+  <twisty-player id="main-player"></twisty-player>
+</body>
+
+</html>
+`,
+);
+await transferFile(
+	"src/main.ts",
+	`// Always keep the following line if you are using any twisty players on your page.
+import "cubing/twisty";
+// Use the following line for specific imports from \`cubing/twisty\`.
+import { TwistyAlgViewer, type TwistyPlayer } from "cubing/twisty";
+
+// Import from other modules as usual.
+import { randomScrambleForEvent } from "cubing/scramble";
+
+class App {
+  // Example of getting an element from the page.
+  twistyPlayer: TwistyPlayer = document.querySelector("#main-player")!;
+  // Example of creating a new element and adding it to the page.
+  twistyAlgViewer = document.body.appendChild(
+    new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer })
+  );
+  constructor() {
+    this.updateScramble();
+  }
+
+  async updateScramble() {
+    this.twistyPlayer.alg = await randomScrambleForEvent("333");
+  }
+}
+
+// Make the app object available in the console for debugging.
+// Try running: app.updateScramble()
+globalThis.app = new App();
+`,
+);
+await transferFile(
+	"src/index.css",
+	`/* Center everything on the page. */
+html, body {
+  height: 100%;
+  margin: 0;
+  display: grid;
+  place-content: center;
+  gap: 1em;
+  font-family: sans-serif;
+}
+`,
+);
+await transferFile(
+	".gitignore",
+	`/dist
+/node_modules
+`,
+);
