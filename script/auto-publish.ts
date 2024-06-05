00ad95215830ceb3269176a87d62eb1ec0981860
Lucas Garron <code@garron.net>
2024-06-05 02:02:40 -0700

    [auto-publish.ts] Remove an extra `console.log(…)`.

--- script/auto-publish.ts
+++ script/auto-publish.ts
@@ -8,7 +8,6 @@ if ((await $`git status --porcelain`).stdout.toString().trim()) {
 
 const version = (await $`npm show cubing version`).stdout.toString().trim();
 
-console.log(new URL(import.meta.resolve("../app-template/")).pathname);
 await $`npm install --prefix ${new URL(import.meta.resolve("../app-template/")).pathname} "cubing@v${version}"`;
 await $`npm version --no-git-tag-version "v${version}"`;
 await $`git commit --all --message "v${version}"`;
