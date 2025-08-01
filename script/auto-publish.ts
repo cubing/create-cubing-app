6074fa9d7c3fc9b7c86d70c3a1360d3d0b5a2029
Lucas Garron <code@garron.net>
2025-07-31 23:04:07 -0700

    [auto-publish.ts] Add an idempotency check.

--- script/auto-publish.ts
+++ script/auto-publish.ts
@@ -8,6 +8,15 @@ if ((await $`git status --porcelain`).stdout.toString().trim()) {
 
 const version = (await $`npm show cubing version`).stdout.toString().trim();
 
+const currentDependencyVersion: string = (
+  await $`cd app-template && npm ls cubing --json`.json()
+).dependencies.cubing.version;
+if (currentDependencyVersion === version) {
+  console.log(`Current version matches latest from \`npm\`: ${version}`);
+  console.log("Exiting (without error).");
+  exit(0);
+}
+
 await $`npm install --prefix ${fileURLToPath(new URL(import.meta.resolve("../app-template/")))} "cubing@v${version}"`;
 await $`npm version --no-git-tag-version "v${version}"`;
 await $`git commit --all --message "v${version}"`;
